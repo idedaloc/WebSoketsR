49f4a3a3bfacb229530a3a3a0e4c2ac759e9c2b5
Jorge <Huser@LTNJ-4Z78GC2.corp.hexaware.com>
2019-05-27 23:52:37 -0400

    Adding websockets code

---
+++ src/main/java/com/websocket/Greeting.java
@@ -0,0 +1,28 @@
+package com.websocket;
+
+public class Greeting {
+	
+	private String greeting;
+
+	public Greeting() {
+		super();
+	}
+
+	
+	public Greeting(String greeting) {
+		super();
+		this.greeting = greeting;
+	}
+
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	
+	
+
+}
